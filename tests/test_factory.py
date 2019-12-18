f726ef4bf185210b24cfe86713032a3fd028f353
LeeW <weilee.rx@gmail.com>
2019-12-18 15:08:52 +0800

    test(all): fix test break due to refactoring
    note that empty config file is not yet handled. should be fix in next commit

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,15 +1,18 @@
 import pytest
 
 from commitizen import BaseCommitizen, defaults, factory
+from commitizen.config import BaseConfig
 
 
 def test_factory():
-    config = {"name": defaults.name}
+    config = BaseConfig()
+    config._settings["name"] = defaults.name
     r = factory.commiter_factory(config)
     assert isinstance(r, BaseCommitizen)
 
 
 def test_factory_fails():
-    config = {"name": "nothing"}
+    config = BaseConfig()
+    config._settings["name"] = "Nothing"
     with pytest.raises(SystemExit):
         factory.commiter_factory(config)
