527ef05d3a3a04926d038a0fb596b08302583a9f
Wei Lee <weilee.rx@gmail.com>
2020-01-17 18:53:42 +0800

    test(commands/bump): add test case for uncovered bump command condition

--- tests/commands/test_bump_command.py
+++ tests/commands/test_bump_command.py
@@ -33,6 +33,7 @@ def create_project():
     full_tmp_path = os.path.join(current_directory, tmp_proj_path)
     if not os.path.exists(full_tmp_path):
         os.makedirs(full_tmp_path)
+
     os.chdir(full_tmp_path)
     yield
     os.chdir(current_directory)
@@ -42,6 +43,7 @@ def create_project():
 def create_file_and_commit(message: str, filename: Optional[str] = None):
     if not filename:
         filename = str(uuid.uuid4())
+
     Path(f"./{filename}").touch()
     cmd.run("git add .")
     git.commit(message)
@@ -102,3 +104,56 @@ def test_bump_command(mocker, create_project):
 
     tag_exists = git.tag_exist("1.0.0")
     assert tag_exists is True
+
+
+def test_bump_when_bumpping_is_not_support(mocker, capsys, tmpdir):
+    with tmpdir.as_cwd():
+        with open("./pyproject.toml", "w") as f:
+            f.write("[tool.commitizen]\n" 'version="0.1.0"')
+
+        cmd.run("git init")
+        create_file_and_commit(
+            "feat: new user interface\n\nBREAKING CHANGE: age is no longer supported"
+        )
+
+        testargs = ["cz", "-n", "cz_jira", "bump", "--yes"]
+        mocker.patch.object(sys, "argv", testargs)
+
+        with pytest.raises(SystemExit):
+            cli.main()
+
+        _, err = capsys.readouterr()
+        assert "'cz_jira' rule does not support bump" in err
+
+
+def test_bump_is_not_specify(mocker, capsys, tmpdir):
+    mocker.patch.object(sys, "argv", ["cz", "bump"])
+
+    with pytest.raises(SystemExit):
+        with tmpdir.as_cwd():
+            cli.main()
+
+    expected_error_message = (
+        "[NO_VERSION_SPECIFIED]\n"
+        "Check if current version is specified in config file, like:\n"
+        "version = 0.4.3\n"
+    )
+
+    _, err = capsys.readouterr()
+    assert expected_error_message in err
+
+
+def test_bump_when_not_new_commit(mocker, capsys, tmpdir):
+    with tmpdir.as_cwd():
+        with open("./pyproject.toml", "w") as f:
+            f.write("[tool.commitizen]\n" 'version="0.1.0"')
+        cmd.run("git init")
+
+        testargs = ["cz", "bump", "--yes"]
+        mocker.patch.object(sys, "argv", testargs)
+
+        with pytest.raises(SystemExit):
+            cli.main()
+    expected_error_message = "[NO_COMMITS_FOUND]\n" "No new commits found."
+    _, err = capsys.readouterr()
+    assert expected_error_message in err
