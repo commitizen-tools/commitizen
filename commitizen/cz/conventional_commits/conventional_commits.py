7eb8afaaf7dd7b55ad98d0fcb4570d0e9ffbc294
Wei Lee <weilee.rx@gmail.com>
2020-02-01 19:38:01 +0800

    fix(commands/check): add bump into valid commit message of convention commit pattern

--- commitizen/cz/conventional_commits/conventional_commits.py
+++ commitizen/cz/conventional_commits/conventional_commits.py
@@ -171,7 +171,7 @@ class ConventionalCommitsCz(BaseCommitizen):
 
     def schema_pattern(self) -> str:
         PATTERN = (
-            r"(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)"
+            r"(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert|bump)"
             r"(\([\w\-]+\))?:\s.*"
         )
         return PATTERN
