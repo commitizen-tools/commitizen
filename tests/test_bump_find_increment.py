4568c269eebb81fd2a5314cb27451171a64ab483
Georgiy Ignatov <vBLFTePebWNi6c@gmail.com>
2020-04-09 10:43:54 +0300

    feat(find_increment): allow to use regex keys for bump_map
    Default bump_map doesnt consider breaking change identificator "!". Regex keys should take care of it.

--- tests/test_bump_find_increment.py
+++ tests/test_bump_find_increment.py
@@ -2,9 +2,11 @@
 CC: Conventional commits
 SVE: Semantic version at the end
 """
+from collections import OrderedDict
+
 import pytest
 
-from commitizen import bump
+from commitizen import bump, defaults
 from commitizen.git import GitCommit
 
 NONE_INCREMENT_CC = ["docs(README): motivation", "ci: added travis"]
@@ -27,6 +29,23 @@ MAJOR_INCREMENTS_CC = [
     "fix(setup.py): future is now required for every python version",
 ]
 
+BUMP_PATTERN_REGEX_CC = r"^(BREAKING[\-\ ]CHANGE|feat|fix|refactor|perf)(\(.+\))?(!)?"
+BUMP_MAP_REGEX_CC = OrderedDict(
+    (
+        (r"^.+!$", defaults.MAJOR),
+        (r"^BREAKING[\-\ ]CHANGE", defaults.MAJOR),
+        (r"^feat", defaults.MINOR),
+        (r"^fix", defaults.PATCH),
+        (r"^refactor", defaults.PATCH),
+        (r"^perf", defaults.PATCH),
+    )
+)
+MAJOR_INCREMENTS_REGEX_CC = [
+    "feat(cli)!: added version",
+    "BREAKING CHANGE: break everything",
+    "BREAKING-CHANGE: alternative breaking change key",
+]
+
 PATCH_INCREMENTS_SVE = ["readme motivation PATCH", "fix setup.py PATCH"]
 
 MINOR_INCREMENTS_SVE = [
@@ -47,17 +66,18 @@ semantic_version_map = {"MAJOR": "MAJOR", "MINOR": "MINOR", "PATCH": "PATCH"}
 
 
 @pytest.mark.parametrize(
-    "messages, expected_type",
+    "messages, expected_type, regex, increment_map",
     (
-        (PATCH_INCREMENTS_CC, "PATCH"),
-        (MINOR_INCREMENTS_CC, "MINOR"),
-        (MAJOR_INCREMENTS_CC, "MAJOR"),
-        (NONE_INCREMENT_CC, None),
+        (PATCH_INCREMENTS_CC, "PATCH", defaults.bump_pattern, defaults.bump_map),
+        (MINOR_INCREMENTS_CC, "MINOR", defaults.bump_pattern, defaults.bump_map),
+        (MAJOR_INCREMENTS_CC, "MAJOR", defaults.bump_pattern, defaults.bump_map),
+        (MAJOR_INCREMENTS_REGEX_CC, "MAJOR", BUMP_PATTERN_REGEX_CC, BUMP_MAP_REGEX_CC),
+        (NONE_INCREMENT_CC, None, defaults.bump_pattern, defaults.bump_map),
     ),
 )
-def test_find_increment(messages, expected_type):
+def test_find_increment(messages, expected_type, regex, increment_map):
     commits = [GitCommit(rev="test", title=message) for message in messages]
-    increment_type = bump.find_increment(commits)
+    increment_type = bump.find_increment(commits, regex, increment_map)
     assert increment_type == expected_type
 
 
