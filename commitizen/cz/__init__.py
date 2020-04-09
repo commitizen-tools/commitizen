9c12b9fbd3e1d8f14b9ba7b542c75f5fe1fee828
Wei Lee <weilee.rx@gmail.com>
2020-04-09 17:03:45 +0800

    refactor(cz): add type annotation to registry

--- commitizen/cz/__init__.py
+++ commitizen/cz/__init__.py
@@ -1,11 +1,13 @@
 import importlib
 import pkgutil
+from typing import Dict, Type
 
+from commitizen.cz.base import BaseCommitizen
 from commitizen.cz.conventional_commits import ConventionalCommitsCz
 from commitizen.cz.customize import CustomizeCommitsCz
 from commitizen.cz.jira import JiraSmartCz
 
-registry = {
+registry: Dict[str, Type[BaseCommitizen]] = {
     "cz_conventional_commits": ConventionalCommitsCz,
     "cz_jira": JiraSmartCz,
     "cz_customize": CustomizeCommitsCz,
