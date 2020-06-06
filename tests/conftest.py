c0b743f31aea3b3cff6825dcd3cad5d9951aee1d
Wei Lee <weilee.rx@gmail.com>
2020-06-06 15:33:31 +0800

    test: move default config to top level conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
-from commitizen import cmd
+from commitizen import cmd, defaults
+from commitizen.config import BaseConfig
 
 
 @pytest.fixture(scope="function")
@@ -18,3 +19,10 @@ def tmp_commitizen_project(tmp_git_project):
         tmp_commitizen_cfg_file.write("[tool.commitizen]\n" 'version="0.1.0"\n')
 
         yield tmp_git_project
+
+
+@pytest.fixture()
+def config():
+    _config = BaseConfig()
+    _config.settings.update({"name": defaults.DEFAULT_SETTINGS["name"]})
+    return _config
