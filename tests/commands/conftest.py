249e7ccc1e1a60303469e9674228bff3c7ceebd8
Jan Willhaus <mail@janwillhaus.de>
2020-11-04 14:13:10 +0100

    test: provide tests for update_changelog_on_bump config option

--- tests/commands/conftest.py
+++ tests/commands/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from commitizen import defaults
@@ -9,3 +11,13 @@ def config():
     _config = BaseConfig()
     _config.settings.update({"name": defaults.name})
     return _config
+
+
+@pytest.fixture()
+def changelog_path() -> str:
+    return os.path.join(os.getcwd(), "CHANGELOG.md")
+
+
+@pytest.fixture()
+def config_path() -> str:
+    return os.path.join(os.getcwd(), "pyproject.toml")
