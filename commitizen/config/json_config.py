9b41945af906f6450f09cd8ec00d8b508c202578
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-11-27 00:24:29 -0600

    feat(commitizen/config/json_config): add json support for configuration
    120

---
+++ commitizen/config/json_config.py
@@ -0,0 +1,48 @@
+import json
+from pathlib import Path
+from typing import Union
+
+from .base_config import BaseConfig
+
+
+class JsonConfig(BaseConfig):
+    def __init__(self, *, data: str, path: Union[Path, str]):
+        super(JsonConfig, self).__init__()
+        self.is_empty_config = False
+        self._parse_setting(data)
+        self.add_path(path)
+
+    def init_empty_config_content(self):
+        with open(self.path, "a") as json_file:
+            json.dump({"commitizen": ""}, json_file)
+
+    def set_key(self, key, value):
+        """Set or update a key in the conf.
+
+        For now only strings are supported.
+        We use to update the version number.
+        """
+        with open(self.path, "r") as f:
+            parser = json.load(f)
+
+        parser["commitizen"][key] = value
+        with open(self.path, "w") as f:
+            json.dump(parser, f)
+        return self
+
+    def _parse_setting(self, data: str):
+        """We expect to have a section in .cz.json looking like
+
+        ```
+        {
+            "commitizen": {
+                "name": "cz_conventional_commits"
+            }
+        }
+        ```
+        """
+        doc = json.loads(data)
+        try:
+            self.settings.update(doc["commitizen"])
+        except KeyError:
+            self.is_empty_config = True
