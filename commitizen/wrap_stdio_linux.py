2a33ba00e3ab3879386ddc596540f6690674bc02
saygox <ito.saygo@gmail.com>
2021-11-19 23:11:18 +0900

    test(wrap_stdio): does not load other platform

--- commitizen/wrap_stdio_linux.py
+++ commitizen/wrap_stdio_linux.py
@@ -1,55 +1,53 @@
-import os
 import sys
-from io import IOBase
 
+if sys.platform == "linux":  # pragma: no cover
+    import os
+    from io import IOBase
 
-class WrapStdioLinux:
-    def __init__(self, stdx: IOBase):
-        self._fileno = stdx.fileno()
-        if self._fileno == 0:
-            fd = os.open("/dev/tty", os.O_RDWR | os.O_NOCTTY)
-            tty = open(fd, "wb+", buffering=0)
-        else:
-            tty = open("/dev/tty", "w")  # type: ignore
-        self.tty = tty
+    class WrapStdioLinux:
+        def __init__(self, stdx: IOBase):
+            self._fileno = stdx.fileno()
+            if self._fileno == 0:
+                fd = os.open("/dev/tty", os.O_RDWR | os.O_NOCTTY)
+                tty = open(fd, "wb+", buffering=0)
+            else:
+                tty = open("/dev/tty", "w")  # type: ignore
+            self.tty = tty
 
-    def __getattr__(self, key):
-        if key == "encoding" and self._fileno == 0:
-            return "UTF-8"
-        return getattr(self.tty, key)
+        def __getattr__(self, key):
+            if key == "encoding" and self._fileno == 0:
+                return "UTF-8"
+            return getattr(self.tty, key)
 
-    def __del__(self):
-        self.tty.close()
+        def __del__(self):
+            self.tty.close()
 
+    backup_stdin = None
+    backup_stdout = None
+    backup_stderr = None
 
-backup_stdin = None
-backup_stdout = None
-backup_stderr = None
+    def _wrap_stdio():
+        global backup_stdin
+        backup_stdin = sys.stdin
+        sys.stdin = WrapStdioLinux(sys.stdin)
 
+        global backup_stdout
+        backup_stdout = sys.stdout
+        sys.stdout = WrapStdioLinux(sys.stdout)
 
-def _wrap_stdio():
-    global backup_stdin
-    backup_stdin = sys.stdin
-    sys.stdin = WrapStdioLinux(sys.stdin)
+        global backup_stderr
+        backup_stderr = sys.stderr
+        sys.stderr = WrapStdioLinux(sys.stderr)
 
-    global backup_stdout
-    backup_stdout = sys.stdout
-    sys.stdout = WrapStdioLinux(sys.stdout)
+    def _unwrap_stdio():
+        global backup_stdin
+        sys.stdin.close()
+        sys.stdin = backup_stdin
 
-    global backup_stderr
-    backup_stderr = sys.stderr
-    sys.stderr = WrapStdioLinux(sys.stderr)
+        global backup_stdout
+        sys.stdout.close()
+        sys.stdout = backup_stdout
 
-
-def _unwrap_stdio():
-    global backup_stdin
-    sys.stdin.close()
-    sys.stdin = backup_stdin
-
-    global backup_stdout
-    sys.stdout.close()
-    sys.stdout = backup_stdout
-
-    global backup_stderr
-    sys.stderr.close()
-    sys.stderr = backup_stderr
+        global backup_stderr
+        sys.stderr.close()
+        sys.stderr = backup_stderr
