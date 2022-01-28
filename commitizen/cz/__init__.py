05aac7433d0334518b994167422c8f664bcb84d3
Manuel <mmartinortiz@gmail.com>
2022-01-28 17:03:11 +0100

    fix: Ignore packages that are not plugins
    When a package starting with a plugin name is found, but it is not a plugin, it becomes safely ignored.

--- commitizen/cz/__init__.py
+++ commitizen/cz/__init__.py
@@ -1,21 +1,30 @@
 import importlib
+import logging
 import pkgutil
-from typing import Dict, Type
 
+from typing import Dict, Type
 from commitizen.cz.base import BaseCommitizen
 from commitizen.cz.conventional_commits import ConventionalCommitsCz
 from commitizen.cz.customize import CustomizeCommitsCz
 from commitizen.cz.jira import JiraSmartCz
+
+logger = logging.getLogger(__name__)
+
+def discover_plugins():
+    plugins = {}
+    for finder, name, ispkg in pkgutil.iter_modules():
+        try:
+            if name.startswith("cz_"):
+                plugins[name] = importlib.import_module(name).discover_this
+        except AttributeError as e:
+            logger.warning(e.args[0])
+            continue
+    return plugins
 
 registry: Dict[str, Type[BaseCommitizen]] = {
     "cz_conventional_commits": ConventionalCommitsCz,
     "cz_jira": JiraSmartCz,
     "cz_customize": CustomizeCommitsCz,
 }
-plugins = {
-    name: importlib.import_module(name).discover_this  # type: ignore
-    for finder, name, ispkg in pkgutil.iter_modules()
-    if name.startswith("cz_")
-}
 
-registry.update(plugins)
+registry.update(discover_plugins())
