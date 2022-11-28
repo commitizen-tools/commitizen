ef75fe59bb8f05e79f64c049134abd4d3fe0011b
Wei Lee <weilee.rx@gmail.com>
2022-11-28 23:01:20 +0800

    test: fix test case changes due to tomlkit upgrade

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -139,8 +139,8 @@ class TestTomlConfig:
         toml_config = config.TomlConfig(data="", path=path)
         toml_config.init_empty_config_content()
 
-        with open(path, "r", encoding="utf-8") as toml_file:
-            assert toml_file.read() == "[tool]\n[tool.commitizen]\n"
+        with open(path, "r") as toml_file:
+            assert toml_file.read() == "[tool.commitizen]\n"
 
     def test_init_empty_config_content_with_existing_content(self, tmpdir):
         existing_content = "[tool.black]\n" "line-length = 88\n"
