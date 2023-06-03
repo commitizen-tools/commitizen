ded9fef36f42ce5bc1159540cfe731d50968205e
Santiago Fraire <santiwilly@gmail.com>
2023-06-03 13:49:45 +0200

    feat: add support for cargo workspaces

--- commitizen/providers.py
+++ commitizen/providers.py
@@ -115,14 +115,25 @@ class PoetryProvider(TomlProvider):
 class CargoProvider(TomlProvider):
     """
     Cargo version management
+
+    With support for `workspaces`
     """
 
     filename = "Cargo.toml"
 
     def get(self, document: tomlkit.TOMLDocument) -> str:
-        return document["package"]["version"]  # type: ignore
+        try:
+            return document["package"]["version"]  # type: ignore
+        except tomlkit.exceptions.NonExistentKey:
+            ...
+        return document["workspace"]["package"]["version"]  # type: ignore
 
     def set(self, document: tomlkit.TOMLDocument, version: str):
+        try:
+            document["workspace"]["package"]["version"] = version  # type: ignore
+            return
+        except tomlkit.exceptions.NonExistentKey:
+            ...
         document["package"]["version"] = version  # type: ignore
 
 
