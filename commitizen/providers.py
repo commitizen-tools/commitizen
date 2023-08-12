8b1cf1e5f5fb1f45ca9e572dc3f1a8c897be8eac
David L. Day <dday376@gmail.com>
2023-08-12 15:43:29 -0400

    feat(provider): add npm2 provider to update package.json, package-lock.json, and npm-shrinkwrap.json
    closes #804

--- commitizen/providers.py
+++ commitizen/providers.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import json
+import jsonpath_ng  # type: ignore
 import re
 from abc import ABC, abstractmethod
 from pathlib import Path
@@ -164,6 +165,87 @@ class NpmProvider(JsonProvider):
     """
 
     filename = "package.json"
+
+
+class Npm2Provider(VersionProvider):
+    """
+    npm package.json and package-lock.json version management
+    """
+
+    indent: ClassVar[int] = 2
+    package_filename = "package.json"
+    package_expression = "$.version"
+    lock_filename = "package-lock.json"
+    lock_expressions = ["$.version", "$.packages.''.version"]
+    shrinkwrap_filename = "npm-shrinkwrap.json"
+    shrinkwrap_expressions = ["$.version", "$.packages.''.version"]
+
+    @property
+    def package_file(self) -> Path:
+        return Path() / self.package_filename
+
+    @property
+    def lock_file(self) -> Path:
+        return Path() / self.lock_filename
+
+    @property
+    def shrinkwrap_file(self) -> Path:
+        return Path() / self.shrinkwrap_filename
+
+    def get_version(self) -> str:
+        """
+        Get the current version from package.json
+        """
+        package_document = json.loads(self.package_file.read_text())
+        return self.get_package_version(package_document)
+
+    def set_version(self, version: str):
+        package_document = self.set_package_version(
+            json.loads(self.package_file.read_text()), version
+        )
+        self.package_file.write_text(
+            json.dumps(package_document, indent=self.indent) + "\n"
+        )
+        if self.lock_file.exists():
+            lock_document = self.set_lock_version(
+                json.loads(self.lock_file.read_text()), version
+            )
+            self.lock_file.write_text(
+                json.dumps(lock_document, indent=self.indent) + "\n"
+            )
+        if self.shrinkwrap_file.exists():
+            shrinkwrap_document = self.set_lock_version(
+                json.loads(self.shrinkwrap_file.read_text()), version
+            )
+            self.shrinkwrap_file.write_text(
+                json.dumps(shrinkwrap_document, indent=self.indent) + "\n"
+            )
+
+    def get_package_version(self, document: dict[str, Any]) -> str:
+        expr = jsonpath_ng.parse(self.package_expression)
+        versions = expr.find(document)
+        if len(versions) == 0:
+            raise ValueError("No version found")
+        elif len(versions) > 1:
+            raise ValueError("Multiple versions found")
+        return versions[0].value  # type: ignore
+
+    def set_package_version(self, document: dict[str, Any], version: str):
+        expr = jsonpath_ng.parse(self.package_expression)
+        document = expr.update(document, version)
+        return document
+
+    def set_lock_version(self, document: dict[str, Any], version: str):
+        for expression in self.lock_expressions:
+            expr = jsonpath_ng.parse(expression)
+            document = expr.update(document, version)
+        return document
+
+    def set_shrinkwrap_version(self, document: dict[str, Any], version: str):
+        for expression in self.shrinkwrap_expressions:
+            expr = jsonpath_ng.parse(expression)
+            document = expr.update(document, version)
+        return document
 
 
 class ComposerProvider(JsonProvider):
