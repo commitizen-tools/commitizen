6eb870429742cfb423610f69f0bb6dfe0070903c
Marcos Martins <contato@marcosmartins.me>
2024-05-16 15:57:09 -0300

    refactor(tests/commands): move "other" tests for the correspondent file

--- tests/commands/test_example_command.py
+++ tests/commands/test_example_command.py
@@ -1,8 +1,14 @@
 import pytest
 import sys
-from commitizen import cli
+from commitizen import cli, commands
 
 from pytest_mock import MockerFixture
+
+
+def test_example(config, mocker: MockerFixture):
+    write_mock = mocker.patch("commitizen.out.write")
+    commands.Example(config)()
+    write_mock.assert_called_once()
 
 
 def test_example_command_shows_description_when_use_help_option(
--- tests/commands/test_ls_command.py
+++ tests/commands/test_ls_command.py
@@ -1,8 +1,14 @@
 import pytest
 import sys
-from commitizen import cli
+from commitizen import cli, commands
 
 from pytest_mock import MockerFixture
+
+
+def test_list_cz(config, mocker: MockerFixture):
+    write_mock = mocker.patch("commitizen.out.write")
+    commands.ListCz(config)()
+    write_mock.assert_called_once()
 
 
 def test_ls_command_shows_description_when_use_help_option(
