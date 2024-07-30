476be7872cf25bbd52dbea4a1301d7066e2260e6
Marcel Blijleven <marcelblijleven@gmail.com>
2024-07-30 13:10:58 +0200

    feat(bump): add functionality to write the next version to stdout
    Adds a --get-next flag which determines the next version and writes it to stdout.

--- tests/commands/test_bump_command.py
+++ tests/commands/test_bump_command.py
@@ -21,6 +21,7 @@ from commitizen.exceptions import (
     DryRunExit,
     ExitCode,
     ExpectedExit,
+    GetNextExit,
     InvalidManualVersion,
     NoCommitsFoundError,
     NoneIncrementExit,
@@ -1462,3 +1463,63 @@ def test_bump_command_shows_description_when_use_help_option(
 
     out, _ = capsys.readouterr()
     file_regression.check(out, extension=".txt")
+
+
+@pytest.mark.usefixtures("tmp_commitizen_project")
+def test_bump_get_next(mocker: MockFixture, capsys):
+    create_file_and_commit("feat: new file")
+
+    testargs = ["cz", "bump", "--yes", "--get-next"]
+    mocker.patch.object(sys, "argv", testargs)
+    with pytest.raises(GetNextExit):
+        cli.main()
+
+    out, _ = capsys.readouterr()
+    assert "0.2.0" in out
+
+    tag_exists = git.tag_exist("0.2.0")
+    assert tag_exists is False
+
+
+@pytest.mark.usefixtures("tmp_commitizen_project")
+def test_bump_get_next__changelog_is_not_allowed(mocker: MockFixture):
+    create_file_and_commit("feat: new file")
+
+    testargs = ["cz", "bump", "--yes", "--get-next", "--changelog"]
+    mocker.patch.object(sys, "argv", testargs)
+
+    with pytest.raises(NotAllowed):
+        cli.main()
+
+
+@pytest.mark.usefixtures("tmp_commitizen_project")
+def test_bump_get_next__changelog_to_stdout_is_not_allowed(mocker: MockFixture):
+    create_file_and_commit("feat: new file")
+
+    testargs = ["cz", "bump", "--yes", "--get-next", "--changelog-to-stdout"]
+    mocker.patch.object(sys, "argv", testargs)
+
+    with pytest.raises(NotAllowed):
+        cli.main()
+
+
+@pytest.mark.usefixtures("tmp_commitizen_project")
+def test_bump_get_next__manual_version_is_not_allowed(mocker: MockFixture):
+    create_file_and_commit("feat: new file")
+
+    testargs = ["cz", "bump", "--yes", "--get-next", "0.2.1"]
+    mocker.patch.object(sys, "argv", testargs)
+
+    with pytest.raises(NotAllowed):
+        cli.main()
+
+
+@pytest.mark.usefixtures("tmp_commitizen_project")
+def test_bump_get_next__no_eligible_commits_raises(mocker: MockFixture):
+    create_file_and_commit("chore: new commit")
+
+    testargs = ["cz", "bump", "--yes", "--get-next"]
+    mocker.patch.object(sys, "argv", testargs)
+
+    with pytest.raises(NoneIncrementExit):
+        cli.main()
