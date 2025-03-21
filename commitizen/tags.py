ffd60a0204c0878081c043ee1e285bd7e2742432
Carlos Sanchez <carlos@apache.org>
2025-03-21 17:32:39 +0100

    fix: print which tag is invalid
    and the regex used for validation

--- commitizen/tags.py
+++ commitizen/tags.py
@@ -146,7 +146,8 @@ class TagRules:
             m for regex in self.version_regexes if (m := regex.fullmatch(tag.name))
         )
         if not (m := next(candidates, None)):
-            raise InvalidVersion()
+            regexes = ", ".join([str(r) for r in self.version_regexes])
+            raise InvalidVersion(f"Invalid version tag: '{tag.name}' does not match any configured tag format: {regexes}")
         if "version" in m.groupdict():
             return self.scheme(m.group("version"))
 
