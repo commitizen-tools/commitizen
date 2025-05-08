ff5ac9a9dce166215cc5303dc9057da322b5ebdb
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2025-05-08 22:55:24 +0300

    feat(providers): add support for `Cargo.lock`
    If `Cargo.lock` file is present in the same path as the `Cargo.toml`,
    the version of the package will also be updated in the `Cargo.lock`
    file.
    
    The implementation is similar to the one used for `UvProvider`, which
    extends the `CargoProvider` to handle lock files. The change does not
    break any existing functionality, if the `Cargo.lock` file is not
    present, since it checks for the existence of the file before
    attempting a lock file update.

--- commitizen/providers/cargo_provider.py
+++ commitizen/providers/cargo_provider.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 import tomlkit
 
 from commitizen.providers.base_provider import TomlProvider
@@ -13,6 +15,11 @@ class CargoProvider(TomlProvider):
     """
 
     filename = "Cargo.toml"
+    lock_filename = "Cargo.lock"
+
+    @property
+    def lock_file(self) -> Path:
+        return Path() / self.lock_filename
 
     def get(self, document: tomlkit.TOMLDocument) -> str:
         try:
@@ -28,3 +35,23 @@ class CargoProvider(TomlProvider):
         except tomlkit.exceptions.NonExistentKey:
             ...
         document["package"]["version"] = version  # type: ignore
+
+    def set_version(self, version: str) -> None:
+        super().set_version(version)
+        if self.lock_file.exists():
+            self.set_lock_version(version)
+
+    def set_lock_version(self, version: str) -> None:
+        cargo_toml_content = tomlkit.parse(self.file.read_text())
+        try:
+            package_name = cargo_toml_content["package"]["name"]  # type: ignore
+        except tomlkit.exceptions.NonExistentKey:
+            package_name = cargo_toml_content["workspace"]["package"]["name"]  # type: ignore
+
+        cargo_lock_content = tomlkit.parse(self.lock_file.read_text())
+        packages: tomlkit.items.AoT = cargo_lock_content["package"]  # type: ignore[assignment]
+        for i, package in enumerate(packages):
+            if package["name"] == package_name:
+                cargo_lock_content["package"][i]["version"] = version  # type: ignore[index]
+                break
+        self.lock_file.write_text(tomlkit.dumps(cargo_lock_content))
