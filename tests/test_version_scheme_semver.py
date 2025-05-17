52acd607a630fbe0a4ae05a865e9bf6c0a525a7e
Yu-Ting Hsiung <bear890707@gmail.com>
2025-05-18 01:25:19 +0800

    feat(bump_rule): add BumpRule, VersionIncrement, Prerelease Enum
    Closes #129

--- tests/test_version_scheme_semver.py
+++ tests/test_version_scheme_semver.py
@@ -3,121 +3,122 @@ import random
 
 import pytest
 
-from commitizen.version_schemes import SemVer, VersionProtocol
+from commitizen.bump_rule import VersionIncrement
+from commitizen.version_schemes import Prerelease, SemVer, VersionProtocol
 
 simple_flow = [
-    (("0.1.0", "PATCH", None, 0, None), "0.1.1"),
-    (("0.1.0", "PATCH", None, 0, 1), "0.1.1-dev1"),
-    (("0.1.1", "MINOR", None, 0, None), "0.2.0"),
-    (("0.2.0", "MINOR", None, 0, None), "0.3.0"),
-    (("0.2.0", "MINOR", None, 0, 1), "0.3.0-dev1"),
-    (("0.3.0", "PATCH", None, 0, None), "0.3.1"),
-    (("0.3.0", "PATCH", "alpha", 0, None), "0.3.1-a0"),
-    (("0.3.1a0", None, "alpha", 0, None), "0.3.1-a1"),
-    (("0.3.0", "PATCH", "alpha", 1, None), "0.3.1-a1"),
-    (("0.3.1a0", None, "alpha", 1, None), "0.3.1-a1"),
+    (("0.1.0", VersionIncrement.PATCH, None, 0, None), "0.1.1"),
+    (("0.1.0", VersionIncrement.PATCH, None, 0, 1), "0.1.1-dev1"),
+    (("0.1.1", VersionIncrement.MINOR, None, 0, None), "0.2.0"),
+    (("0.2.0", VersionIncrement.MINOR, None, 0, None), "0.3.0"),
+    (("0.2.0", VersionIncrement.MINOR, None, 0, 1), "0.3.0-dev1"),
+    (("0.3.0", VersionIncrement.PATCH, None, 0, None), "0.3.1"),
+    (("0.3.0", VersionIncrement.PATCH, Prerelease.ALPHA, 0, None), "0.3.1-a0"),
+    (("0.3.1a0", None, Prerelease.ALPHA, 0, None), "0.3.1-a1"),
+    (("0.3.0", VersionIncrement.PATCH, Prerelease.ALPHA, 1, None), "0.3.1-a1"),
+    (("0.3.1a0", None, Prerelease.ALPHA, 1, None), "0.3.1-a1"),
     (("0.3.1a0", None, None, 0, None), "0.3.1"),
-    (("0.3.1", "PATCH", None, 0, None), "0.3.2"),
-    (("0.4.2", "MAJOR", "alpha", 0, None), "1.0.0-a0"),
-    (("1.0.0a0", None, "alpha", 0, None), "1.0.0-a1"),
-    (("1.0.0a1", None, "alpha", 0, None), "1.0.0-a2"),
-    (("1.0.0a1", None, "alpha", 0, 1), "1.0.0-a2-dev1"),
-    (("1.0.0a2.dev0", None, "alpha", 0, 1), "1.0.0-a3-dev1"),
-    (("1.0.0a2.dev0", None, "alpha", 0, 0), "1.0.0-a3-dev0"),
-    (("1.0.0a1", None, "beta", 0, None), "1.0.0-b0"),
-    (("1.0.0b0", None, "beta", 0, None), "1.0.0-b1"),
-    (("1.0.0b1", None, "rc", 0, None), "1.0.0-rc0"),
-    (("1.0.0rc0", None, "rc", 0, None), "1.0.0-rc1"),
-    (("1.0.0rc0", None, "rc", 0, 1), "1.0.0-rc1-dev1"),
-    (("1.0.0rc0", "PATCH", None, 0, None), "1.0.0"),
-    (("1.0.0a3.dev0", None, "beta", 0, None), "1.0.0-b0"),
-    (("1.0.0", "PATCH", None, 0, None), "1.0.1"),
-    (("1.0.1", "PATCH", None, 0, None), "1.0.2"),
-    (("1.0.2", "MINOR", None, 0, None), "1.1.0"),
-    (("1.1.0", "MINOR", None, 0, None), "1.2.0"),
-    (("1.2.0", "PATCH", None, 0, None), "1.2.1"),
-    (("1.2.1", "MAJOR", None, 0, None), "2.0.0"),
+    (("0.3.1", VersionIncrement.PATCH, None, 0, None), "0.3.2"),
+    (("0.4.2", VersionIncrement.MAJOR, Prerelease.ALPHA, 0, None), "1.0.0-a0"),
+    (("1.0.0a0", None, Prerelease.ALPHA, 0, None), "1.0.0-a1"),
+    (("1.0.0a1", None, Prerelease.ALPHA, 0, None), "1.0.0-a2"),
+    (("1.0.0a1", None, Prerelease.ALPHA, 0, 1), "1.0.0-a2-dev1"),
+    (("1.0.0a2.dev0", None, Prerelease.ALPHA, 0, 1), "1.0.0-a3-dev1"),
+    (("1.0.0a2.dev0", None, Prerelease.ALPHA, 0, 0), "1.0.0-a3-dev0"),
+    (("1.0.0a1", None, Prerelease.BETA, 0, None), "1.0.0-b0"),
+    (("1.0.0b0", None, Prerelease.BETA, 0, None), "1.0.0-b1"),
+    (("1.0.0b1", None, Prerelease.RC, 0, None), "1.0.0-rc0"),
+    (("1.0.0rc0", None, Prerelease.RC, 0, None), "1.0.0-rc1"),
+    (("1.0.0rc0", None, Prerelease.RC, 0, 1), "1.0.0-rc1-dev1"),
+    (("1.0.0rc0", VersionIncrement.PATCH, None, 0, None), "1.0.0"),
+    (("1.0.0a3.dev0", None, Prerelease.BETA, 0, None), "1.0.0-b0"),
+    (("1.0.0", VersionIncrement.PATCH, None, 0, None), "1.0.1"),
+    (("1.0.1", VersionIncrement.PATCH, None, 0, None), "1.0.2"),
+    (("1.0.2", VersionIncrement.MINOR, None, 0, None), "1.1.0"),
+    (("1.1.0", VersionIncrement.MINOR, None, 0, None), "1.2.0"),
+    (("1.2.0", VersionIncrement.PATCH, None, 0, None), "1.2.1"),
+    (("1.2.1", VersionIncrement.MAJOR, None, 0, None), "2.0.0"),
 ]
 
 local_versions = [
-    (("4.5.0+0.1.0", "PATCH", None, 0, None), "4.5.0+0.1.1"),
-    (("4.5.0+0.1.1", "MINOR", None, 0, None), "4.5.0+0.2.0"),
-    (("4.5.0+0.2.0", "MAJOR", None, 0, None), "4.5.0+1.0.0"),
+    (("4.5.0+0.1.0", VersionIncrement.PATCH, None, 0, None), "4.5.0+0.1.1"),
+    (("4.5.0+0.1.1", VersionIncrement.MINOR, None, 0, None), "4.5.0+0.2.0"),
+    (("4.5.0+0.2.0", VersionIncrement.MAJOR, None, 0, None), "4.5.0+1.0.0"),
 ]
 
 # never bump backwards on pre-releases
 linear_prerelease_cases = [
-    (("0.1.1b1", None, "alpha", 0, None), "0.1.1-b2"),
-    (("0.1.1rc0", None, "alpha", 0, None), "0.1.1-rc1"),
-    (("0.1.1rc0", None, "beta", 0, None), "0.1.1-rc1"),
+    (("0.1.1b1", None, Prerelease.ALPHA, 0, None), "0.1.1-b2"),
+    (("0.1.1rc0", None, Prerelease.ALPHA, 0, None), "0.1.1-rc1"),
+    (("0.1.1rc0", None, Prerelease.BETA, 0, None), "0.1.1-rc1"),
 ]
 
 weird_cases = [
-    (("1.1", "PATCH", None, 0, None), "1.1.1"),
-    (("1", "MINOR", None, 0, None), "1.1.0"),
-    (("1", "MAJOR", None, 0, None), "2.0.0"),
-    (("1a0", None, "alpha", 0, None), "1.0.0-a1"),
-    (("1a0", None, "alpha", 1, None), "1.0.0-a1"),
-    (("1", None, "beta", 0, None), "1.0.0-b0"),
-    (("1", None, "beta", 1, None), "1.0.0-b1"),
-    (("1beta", None, "beta", 0, None), "1.0.0-b1"),
-    (("1.0.0alpha1", None, "alpha", 0, None), "1.0.0-a2"),
-    (("1", None, "rc", 0, None), "1.0.0-rc0"),
-    (("1.0.0rc1+e20d7b57f3eb", "PATCH", None, 0, None), "1.0.0"),
+    (("1.1", VersionIncrement.PATCH, None, 0, None), "1.1.1"),
+    (("1", VersionIncrement.MINOR, None, 0, None), "1.1.0"),
+    (("1", VersionIncrement.MAJOR, None, 0, None), "2.0.0"),
+    (("1a0", None, Prerelease.ALPHA, 0, None), "1.0.0-a1"),
+    (("1a0", None, Prerelease.ALPHA, 1, None), "1.0.0-a1"),
+    (("1", None, Prerelease.BETA, 0, None), "1.0.0-b0"),
+    (("1", None, Prerelease.BETA, 1, None), "1.0.0-b1"),
+    (("1beta", None, Prerelease.BETA, 0, None), "1.0.0-b1"),
+    (("1.0.0alpha1", None, Prerelease.ALPHA, 0, None), "1.0.0-a2"),
+    (("1", None, Prerelease.RC, 0, None), "1.0.0-rc0"),
+    (("1.0.0rc1+e20d7b57f3eb", VersionIncrement.PATCH, None, 0, None), "1.0.0"),
 ]
 
 # test driven development
 tdd_cases = [
-    (("0.1.1", "PATCH", None, 0, None), "0.1.2"),
-    (("0.1.1", "MINOR", None, 0, None), "0.2.0"),
-    (("2.1.1", "MAJOR", None, 0, None), "3.0.0"),
-    (("0.9.0", "PATCH", "alpha", 0, None), "0.9.1-a0"),
-    (("0.9.0", "MINOR", "alpha", 0, None), "0.10.0-a0"),
-    (("0.9.0", "MAJOR", "alpha", 0, None), "1.0.0-a0"),
-    (("0.9.0", "MAJOR", "alpha", 1, None), "1.0.0-a1"),
-    (("1.0.0a2", None, "beta", 0, None), "1.0.0-b0"),
-    (("1.0.0a2", None, "beta", 1, None), "1.0.0-b1"),
-    (("1.0.0beta1", None, "rc", 0, None), "1.0.0-rc0"),
-    (("1.0.0rc1", None, "rc", 0, None), "1.0.0-rc2"),
-    (("1.0.0-a0", None, "rc", 0, None), "1.0.0-rc0"),
-    (("1.0.0-alpha1", None, "alpha", 0, None), "1.0.0-a2"),
+    (("0.1.1", VersionIncrement.PATCH, None, 0, None), "0.1.2"),
+    (("0.1.1", VersionIncrement.MINOR, None, 0, None), "0.2.0"),
+    (("2.1.1", VersionIncrement.MAJOR, None, 0, None), "3.0.0"),
+    (("0.9.0", VersionIncrement.PATCH, Prerelease.ALPHA, 0, None), "0.9.1-a0"),
+    (("0.9.0", VersionIncrement.MINOR, Prerelease.ALPHA, 0, None), "0.10.0-a0"),
+    (("0.9.0", VersionIncrement.MAJOR, Prerelease.ALPHA, 0, None), "1.0.0-a0"),
+    (("0.9.0", VersionIncrement.MAJOR, Prerelease.ALPHA, 1, None), "1.0.0-a1"),
+    (("1.0.0a2", None, Prerelease.BETA, 0, None), "1.0.0-b0"),
+    (("1.0.0a2", None, Prerelease.BETA, 1, None), "1.0.0-b1"),
+    (("1.0.0beta1", None, Prerelease.RC, 0, None), "1.0.0-rc0"),
+    (("1.0.0rc1", None, Prerelease.RC, 0, None), "1.0.0-rc2"),
+    (("1.0.0-a0", None, Prerelease.RC, 0, None), "1.0.0-rc0"),
+    (("1.0.0-alpha1", None, Prerelease.ALPHA, 0, None), "1.0.0-a2"),
 ]
 
 exact_cases = [
-    (("1.0.0", "PATCH", None, 0, None), "1.0.1"),
-    (("1.0.0", "MINOR", None, 0, None), "1.1.0"),
+    (("1.0.0", VersionIncrement.PATCH, None, 0, None), "1.0.1"),
+    (("1.0.0", VersionIncrement.MINOR, None, 0, None), "1.1.0"),
     # with exact_increment=False: "1.0.0-b0"
-    (("1.0.0a1", "PATCH", "beta", 0, None), "1.0.1-b0"),
+    (("1.0.0a1", VersionIncrement.PATCH, Prerelease.BETA, 0, None), "1.0.1-b0"),
     # with exact_increment=False: "1.0.0-b1"
-    (("1.0.0b0", "PATCH", "beta", 0, None), "1.0.1-b0"),
+    (("1.0.0b0", VersionIncrement.PATCH, Prerelease.BETA, 0, None), "1.0.1-b0"),
     # with exact_increment=False: "1.0.0-rc0"
-    (("1.0.0b1", "PATCH", "rc", 0, None), "1.0.1-rc0"),
+    (("1.0.0b1", VersionIncrement.PATCH, Prerelease.RC, 0, None), "1.0.1-rc0"),
     # with exact_increment=False: "1.0.0-rc1"
-    (("1.0.0rc0", "PATCH", "rc", 0, None), "1.0.1-rc0"),
+    (("1.0.0rc0", VersionIncrement.PATCH, Prerelease.RC, 0, None), "1.0.1-rc0"),
     # with exact_increment=False: "1.0.0-rc1-dev1"
-    (("1.0.0rc0", "PATCH", "rc", 0, 1), "1.0.1-rc0-dev1"),
+    (("1.0.0rc0", VersionIncrement.PATCH, Prerelease.RC, 0, 1), "1.0.1-rc0-dev1"),
     # with exact_increment=False: "1.0.0-b0"
-    (("1.0.0a1", "MINOR", "beta", 0, None), "1.1.0-b0"),
+    (("1.0.0a1", VersionIncrement.MINOR, Prerelease.BETA, 0, None), "1.1.0-b0"),
     # with exact_increment=False: "1.0.0-b1"
-    (("1.0.0b0", "MINOR", "beta", 0, None), "1.1.0-b0"),
+    (("1.0.0b0", VersionIncrement.MINOR, Prerelease.BETA, 0, None), "1.1.0-b0"),
     # with exact_increment=False: "1.0.0-rc0"
-    (("1.0.0b1", "MINOR", "rc", 0, None), "1.1.0-rc0"),
+    (("1.0.0b1", VersionIncrement.MINOR, Prerelease.RC, 0, None), "1.1.0-rc0"),
     # with exact_increment=False: "1.0.0-rc1"
-    (("1.0.0rc0", "MINOR", "rc", 0, None), "1.1.0-rc0"),
+    (("1.0.0rc0", VersionIncrement.MINOR, Prerelease.RC, 0, None), "1.1.0-rc0"),
     # with exact_increment=False: "1.0.0-rc1-dev1"
-    (("1.0.0rc0", "MINOR", "rc", 0, 1), "1.1.0-rc0-dev1"),
+    (("1.0.0rc0", VersionIncrement.MINOR, Prerelease.RC, 0, 1), "1.1.0-rc0-dev1"),
     # with exact_increment=False: "2.0.0"
-    (("2.0.0b0", "MAJOR", None, 0, None), "3.0.0"),
+    (("2.0.0b0", VersionIncrement.MAJOR, None, 0, None), "3.0.0"),
     # with exact_increment=False: "2.0.0"
-    (("2.0.0b0", "MINOR", None, 0, None), "2.1.0"),
+    (("2.0.0b0", VersionIncrement.MINOR, None, 0, None), "2.1.0"),
     # with exact_increment=False: "2.0.0"
-    (("2.0.0b0", "PATCH", None, 0, None), "2.0.1"),
+    (("2.0.0b0", VersionIncrement.PATCH, None, 0, None), "2.0.1"),
     # same with exact_increment=False
-    (("2.0.0b0", "MAJOR", "alpha", 0, None), "3.0.0-a0"),
+    (("2.0.0b0", VersionIncrement.MAJOR, Prerelease.ALPHA, 0, None), "3.0.0-a0"),
     # with exact_increment=False: "2.0.0b1"
-    (("2.0.0b0", "MINOR", "alpha", 0, None), "2.1.0-a0"),
+    (("2.0.0b0", VersionIncrement.MINOR, Prerelease.ALPHA, 0, None), "2.1.0-a0"),
     # with exact_increment=False: "2.0.0b1"
-    (("2.0.0b0", "PATCH", "alpha", 0, None), "2.0.1-a0"),
+    (("2.0.0b0", VersionIncrement.PATCH, Prerelease.ALPHA, 0, None), "2.0.1-a0"),
 ]
 
 
