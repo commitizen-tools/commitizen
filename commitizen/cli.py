b773bdcd1be2d28eedbee93a021dca6bdce49c6f
Yu-Ting Hsiung <bear890707@gmail.com>
2025-05-31 00:45:15 +0800

    refactor(bump): TypedDict for bump argument

--- commitizen/cli.py
+++ commitizen/cli.py
@@ -682,7 +682,7 @@ def main() -> None:
         )
         sys.excepthook = no_raise_debug_excepthook
 
-    args.func(conf, arguments)()
+    args.func(conf, arguments)()  # type: ignore
 
 
 if __name__ == "__main__":
