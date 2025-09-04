88121152d80331714bdcae68b1f7902893b48009
vemilano <230173219+vemilano@users.noreply.github.com>
2025-09-04 15:24:21 +0200

    fix: cargo workspaces

--- commitizen/providers/cargo_provider.py
+++ commitizen/providers/cargo_provider.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import glob
 from pathlib import Path
 
 import tomlkit
@@ -22,8 +23,10 @@ class CargoProvider(TomlProvider):
         return Path() / self.lock_filename
 
     def get(self, document: tomlkit.TOMLDocument) -> str:
+        # If there is a root package, change its version (but not the workspace version)
         try:
             return document["package"]["version"]  # type: ignore[index,return-value]
+        # Else, bump the workspace version
         except tomlkit.exceptions.NonExistentKey:
             ...
         return document["workspace"]["package"]["version"]  # type: ignore[index,return-value]
@@ -43,15 +46,39 @@ class CargoProvider(TomlProvider):
 
     def set_lock_version(self, version: str) -> None:
         cargo_toml_content = tomlkit.parse(self.file.read_text())
-        try:
-            package_name = cargo_toml_content["package"]["name"]  # type: ignore[index]
-        except tomlkit.exceptions.NonExistentKey:
-            package_name = cargo_toml_content["workspace"]["package"]["name"]  # type: ignore[index]
-
         cargo_lock_content = tomlkit.parse(self.lock_file.read_text())
         packages: tomlkit.items.AoT = cargo_lock_content["package"]  # type: ignore[assignment]
-        for i, package in enumerate(packages):
-            if package["name"] == package_name:
-                cargo_lock_content["package"][i]["version"] = version  # type: ignore[index]
-                break
+        try:
+            package_name = cargo_toml_content["package"]["name"]  # type: ignore[index]
+            for i, package in enumerate(packages):
+                if package["name"] == package_name:
+                    cargo_lock_content["package"][i]["version"] = version  # type: ignore[index]
+                    break
+        except tomlkit.exceptions.NonExistentKey:
+            workspace_members = (
+                cargo_toml_content.get("workspace", {})
+                .get("package", {})
+                .get("members", [])
+            )
+            members_inheriting = []
+
+            for member in workspace_members:
+                matched_paths = glob.glob(member, recursive=True)
+                for path in matched_paths:
+                    cargo_file = Path(path) / "Cargo.toml"
+                    cargo_toml_content = tomlkit.parse(cargo_file.read_text())
+                    try:
+                        version_workspace = cargo_toml_content["package"]["version"][  # type: ignore[index]
+                            "workspace"
+                        ]
+                        if version_workspace is True:
+                            package_name = cargo_toml_content["package"]["name"]  # type: ignore[index]
+                            members_inheriting.append(package_name)
+                    except tomlkit.exceptions.NonExistentKey:
+                        continue
+
+            for i, package in enumerate(packages):
+                if package["name"] in members_inheriting:
+                    cargo_lock_content["package"][i]["version"] = version  # type: ignore[index]
+
         self.lock_file.write_text(tomlkit.dumps(cargo_lock_content))
