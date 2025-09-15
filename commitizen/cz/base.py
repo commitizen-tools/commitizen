2626e011a9d9ac9691f75ce44a7566e8aa7c6304
Yu-Ting Hsiung <bear890707@gmail.com>
2025-09-15 19:44:16 +0800

    refactor(BaseCommitizen): construct Style object directly to get rid of potential type error

--- commitizen/cz/base.py
+++ commitizen/cz/base.py
@@ -5,7 +5,7 @@ from collections.abc import Iterable, Mapping
 from typing import Any, Callable, Protocol
 
 from jinja2 import BaseLoader, PackageLoader
-from prompt_toolkit.styles import Style, merge_styles
+from prompt_toolkit.styles import Style
 
 from commitizen import git
 from commitizen.config.base_config import BaseConfig
@@ -77,12 +77,12 @@ class BaseCommitizen(metaclass=ABCMeta):
 
     @property
     def style(self) -> Style:
-        return merge_styles(
+        return Style(
             [
-                Style(BaseCommitizen.default_style_config),
-                Style(self.config.settings["style"]),
+                *BaseCommitizen.default_style_config,
+                *self.config.settings["style"],
             ]
-        )  # type: ignore[return-value]
+        )
 
     def example(self) -> str:
         """Example of the commit message."""
