5804fca663c354856504cfb0bde74f12666808f1
Yu-Ting Hsiung <bear890707@gmail.com>
2025-11-22 16:35:16 +0800

    fix(version): fix the behavior of cz version --major

--- commitizen/commands/version.py
+++ commitizen/commands/version.py
@@ -11,6 +11,7 @@ from commitizen.version_schemes import get_version_scheme
 
 
 class VersionArgs(TypedDict, total=False):
+    commitizen: bool
     report: bool
     project: bool
     verbose: bool
@@ -19,46 +20,58 @@ class VersionArgs(TypedDict, total=False):
 
 
 class Version:
-    """Get the version of the installed commitizen or the current project."""
+    """Get the version of the installed commitizen or the current project.
+    Precedence:
+    1. report
+    2. commitizen
+    3. verbose, project
+    """
 
     def __init__(self, config: BaseConfig, arguments: VersionArgs) -> None:
         self.config: BaseConfig = config
         self.parameter = arguments
-        self.operating_system = platform.system()
-        self.python_version = sys.version
 
     def __call__(self) -> None:
         if self.parameter.get("report"):
             out.write(f"Commitizen Version: {__version__}")
-            out.write(f"Python Version: {self.python_version}")
-            out.write(f"Operating System: {self.operating_system}")
+            out.write(f"Python Version: {sys.version}")
+            out.write(f"Operating System: {platform.system()}")
             return
 
-        if (verbose := self.parameter.get("verbose")) or self.parameter.get("project"):
-            if verbose:
-                out.write(f"Installed Commitizen Version: {__version__}")
+        if self.parameter.get("verbose"):
+            out.write(f"Installed Commitizen Version: {__version__}")
 
+        if not self.parameter.get("commitizen") and (
+            self.parameter.get("project") or self.parameter.get("verbose")
+        ):
             try:
                 version = get_provider(self.config).get_version()
             except NoVersionSpecifiedError:
                 out.error("No project information in this project.")
                 return
-
             try:
-                version_scheme = get_version_scheme(self.config.settings)
+                version_scheme = get_version_scheme(self.config.settings)(version)
             except VersionSchemeUnknown:
                 out.error("Unknown version scheme.")
                 return
 
-            _version = version_scheme(version)
-
             if self.parameter.get("major"):
-                version = f"{_version.major}"
+                version = f"{version_scheme.major}"
             elif self.parameter.get("minor"):
-                version = f"{_version.minor}"
+                version = f"{version_scheme.minor}"
 
-            out.write(f"Project Version: {version}" if verbose else version)
+            out.write(
+                f"Project Version: {version}"
+                if self.parameter.get("verbose")
+                else version
+            )
             return
 
-        # if no argument is given, show installed commitizen version
-        out.write(f"{__version__}")
+        if self.parameter.get("major") or self.parameter.get("minor"):
+            out.error(
+                "Major or minor version can only be used with --project or --verbose."
+            )
+            return
+
+        # If no arguments are provided, just show the installed commitizen version
+        out.write(__version__)
